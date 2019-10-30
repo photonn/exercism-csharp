1d1a46515901242f7bc013f33adeea421d58554b
photonn <photonn@hotmail.com>
2019-10-30 11:53:16 +0100

    nucleotide count solution

--- nucleotide-count/NucleotideCount.cs
+++ nucleotide-count/NucleotideCount.cs
@@ -3,8 +3,21 @@ using System.Collections.Generic;
 
 public static class NucleotideCount
 {
-    public static IDictionary<char, int> Count(string sequence)
-    {
-        throw new NotImplementedException("You need to implement this function.");
+    public static Dictionary<char, int> Count(string sequence){
+        try{
+            Dictionary<char, int> nucleotides = new Dictionary<char, int>();
+            char[] indNucleotids = sequence.ToCharArray();
+            nucleotides.Add('A',0);
+            nucleotides.Add('C',0);
+            nucleotides.Add('G',0);
+            nucleotides.Add('T',0);
+            foreach(char nucleotide in indNucleotids){
+                nucleotides[nucleotide] =  nucleotides[nucleotide] + 1;
+            }
+            return nucleotides;
+        }
+        catch{
+            throw new ArgumentException();
+        }
     }
 }
