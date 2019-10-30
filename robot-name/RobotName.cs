070b6bb18930b1554adf563b9fd91a4c6c11e70a
photonn <photonn@hotmail.com>
2019-10-30 14:12:31 +0100

    aefjwpo

--- robot-name/RobotName.cs
+++ robot-name/RobotName.cs
@@ -2,16 +2,30 @@ using System;
 
 public class Robot
 {
+    private string name;
     public string Name
     {
         get
         {
-            throw new NotImplementedException("You need to implement this function.");
+            return name;
+        }
+        private set{
+            char[] chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            Random random = new Random(); 
+            string tempName = "";
+            for(int position=0;position<=1;position++){
+                tempName = tempName + chars[random.Next(0, chars.Length-1)].ToString().ToUpper();
+            }
+            tempName = tempName + random.Next(0,999).ToString();
+            name = tempName;
         }
     }
-
+    public Robot()
+    {
+        Reset();
+    }
     public void Reset()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        Name = "";
     }
 }
