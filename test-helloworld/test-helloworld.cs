7ecef6b8372340419d38e54d1e52c1b59e3580a9
photonn <photonn@hotmail.com>
2019-11-05 17:43:40 +0100

    Finally debugs works...

---
+++ test-helloworld/test-helloworld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace test_helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Finally motherfucker, I can debuuuuuuuuuug!!!");
+        }
+    }
+}
